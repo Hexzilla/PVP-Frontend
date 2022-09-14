90e3853c6e2e69ddc9befca62ff696178f05aaca
Eric Thomas <bluebird6922@gmail.com>
2022-09-14 12:10:27 +0300

    theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import { AppProps } from 'next/app';
-import '../styles/globals.css'
-import { NetworkOptions, TezosProvider } from '../contexts/TezosContext';
+import React from "react";
+import { AppProps } from "next/app";
+import { ThemeProvider } from '@mui/material/styles';
+import "../styles/globals.css";
+import { createTheme } from '../theme';
+import { NetworkOptions, TezosProvider } from "../contexts/TezosContext";
 
 const options = {
-  appName: 'PiXL',
-  networkType: 'jakartanet',
-  rpc: 'https://jakartanet.tezos.marigold.dev',
+  appName: "PiXL",
+  networkType: "jakartanet",
+  rpc: "https://jakartanet.tezos.marigold.dev",
 } as NetworkOptions;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <TezosProvider options={options}>
-      <Component {...pageProps} />
-    </TezosProvider>
-  )
+    <ThemeProvider
+      theme={createTheme({
+        direction: 'ltr',
+        responsiveFontSizes: true,
+        mode: 'dark',
+      })}
+    >
+      <TezosProvider options={options}>
+        <Component {...pageProps} />
+      </TezosProvider>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
